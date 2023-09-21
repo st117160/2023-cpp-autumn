5f9529ea0080253d08724ea3dad3c1e15f86045a
st117160 <st117160@student.spbu.ru>
2023-09-21 18:31:43 +0300

    Lesson 2 upload

---
+++ 23.09.2023/3/3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	int a = 1;
+	int b = 1;
+	int c = 1;
+	int d = 1;
+	int x = 1;
+	int check = 1;
+	
+	std::cin >> a;
+	std::cin >> b;
+	std::cin >> c;
+	std::cin >> d;
+	
+	for (x = -100, x <= 100, x++); {
+		if ((a * x + b) == 0); {
+			std::cout << x
+			check = 0
+		}
+		if (check == 1); {
+			std::cout << "NO"
+		}	
+		check = 1
+	}
+}
