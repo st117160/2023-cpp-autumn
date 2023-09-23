d48b84b4ea79c8d810846bc0c25948d079642ec5
st117160 <st117160@student.spbu.ru>
2023-09-23 22:15:10 +0300

    Lesson 2 code upload

--- 23.09.2023/2/2.cpp
+++ 23.09.2023/2/2.cpp
@@ -8,42 +8,14 @@ int main(int argc, char *argv[])
     std::cin >> sys;
     std::cin >> stu;
     
-    if (stu == 1)
+    if ((sys == 1 && stu == 1) || (sys != 1 && stu != 1))
     {
-    	if ((sys % 10) == ((sys - sys % 1000)/1000))
-		{
-			if (((sys%100 - sys % 10)/10) == ((sys % 1000 - sys % 100)/100)) 
-			{
-				std::cout << "YES";
-			}
-			
-			else
-			{
-				std::cout << "NO";
-			}
-		}
-		
-		else
-		{
-			std::cout << "NO";
-		}
+    	std::cout << "YES";
 	}
+	
 	else
 	{
-		if ((sys % 10 != ((sys - sys % 1000)/1000))) 
-		{
-			std::cout << "YES";
-		}
-		
-		else if ((((sys%100 - sys % 10)/10) != ((sys % 1000 - sys % 100)/100)))
-		{
-			std::cout << "YES";
-		}
-		
-		else
-		{
-			std::cout << "NO";
-		}
+		std::cout << "NO";
 	}
 	
 	return 0;
