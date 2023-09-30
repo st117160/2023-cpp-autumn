4d16211a5d1119146d3b24cea8d6719e0d78c460
st117160 <st117160@student.spbu.ru>
2023-10-01 02:02:04 +0300

    Lesson 3 code upload

--- 2023.10.01/8/8.cpp
+++ 2023.10.01/8/8.cpp
@@ -5,35 +5,70 @@ int main(int argc, char *argv[])
 	int a = 0;
 	int b = 0;
 	int n = 0;
-	int x = 0;
 	int gcd = 0;
+	int x = 0;
+	int y = 0;
+	char lit = 'A';
+	char big = 'B';
+	int ost = 0;
 	
 	std::cin >> a;
 	std::cin >> b;
 	std::cin >> n;
 	
-	if (b > a)
-	{
-		x = b;
-		b = a;
-		a = x;	
-	}
+	x = a;
+	y = b;
+	
+	if (a > b)
+    {
+        gcd = a;
+        a = b;
+        b = gcd;
+        lit = 'B';
+        big = 'A';
+    }
 
+    if (n == b)
+    {
+        std::cout << ">" << big;
+        std::cout << std::endl;
+    }
+    
+    while (y != 0) 
+    {
+        gcd = x % y;
+        x = y;
+        y = gcd;
+    }
 	
-	while (a != 0 && b != 0)
-	{
-		if (a > b)
-		{
-			a = a % b;
-		}
-		else
-		{
-			b = b % a;
-		}
-		gcd = a + b;
-	}
-	
-	std::cout << gcd;
-	
-	return 0;
+	if ((n % x != 0) || ((n > a) && (n > b)))
+    {
+        std::cout << "Impossible";
+        std::cout << std::endl;
+    }
+    else
+    {
+        while ('True')
+        {
+            ost += a;
+            std::cout << ">" << lit;
+            std::cout << std::endl;
+            std::cout << lit << ">" << big;
+            std::cout << std::endl;
+            if (ost >= b)
+            {
+                std::cout << big << ">" ;
+				std::cout << std::endl;
+                std::cout << lit << ">" << big;
+                std::cout << std::endl;
+                ost %= b;
+            }
+            
+            if ((ost == n) || (ost == 0))
+            {
+                break;
+            }
+        }
+    }
+    return 0;
 }
